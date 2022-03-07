49f75d07d04f2996164b78d46d8f02833ec87e4b
Carlos Sanabria <Sanabria26mc@gmail.com>
2022-03-07 14:59:37 -0800

    commit tree ebe2854d4c7ea652944de14b50c1b3de98d20a32

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    // const driver1 = drivers[0];
+    // const driver2 = drivers[1];
+   //  return [driver1, driver2];
+       return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    // const last = drivers.pop();
+    // const nextToLast = drivers.pop();
+    // return [nextToLast, last];
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier){
+    return function (fare) {
+        return fare * multiplier;
+    };
+};
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2) (fare);
+};
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3) (fare);
+};
+
+function selectDifferentDrivers(arrayOfDrivers, returnDriversFunction){
+    if (returnDriversFunction){
+        if (returnDriversFunction === returnFirstTwoDrivers){
+            return returnFirstTwoDrivers(arrayOfDrivers);
+            } else if(returnDriversFunction === returnLastTwoDrivers){
+           return returnLastTwoDrivers(arrayOfDrivers);
+            }
+        }
+    }
+
+
+
